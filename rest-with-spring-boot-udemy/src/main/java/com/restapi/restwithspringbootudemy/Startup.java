d0b379cdf24adbd374a583c62a18dad324e5febc
ghaith <ghaith.sabara@gmail.com>
2020-12-09 10:36:10 +0100

    Update Start

--- rest-with-spring-boot-udemy/src/main/java/com/restapi/restwithspringbootudemy/Startup.java
+++ rest-with-spring-boot-udemy/src/main/java/com/restapi/restwithspringbootudemy/Startup.java
@@ -10,23 +10,24 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class Startup {
 
-//	@Autowired
-//	UserRepository userRepository;
-//
-//	@Autowired
-//	private PasswordEncoder passwordEncoder;
+	// @Autowired
+	// UserRepository userRepository;
+	//
+	// @Autowired
+	// private PasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
+		System.out.println("Start...");
 		SpringApplication.run(Startup.class, args);
 	}
 
-//	@Override
-//	public void run(String... params) throws Exception {
-//		User admin = new User();
-//		admin.setUsername("ghaith");
-//		admin.setPassword(passwordEncoder.encode("ghaith"));
-//		admin.setEmail("ghaith@email.com");
-//
-//		userRepository.save(admin);
-//	}
+	// @Override
+	// public void run(String... params) throws Exception {
+	// User admin = new User();
+	// admin.setUsername("ghaith");
+	// admin.setPassword(passwordEncoder.encode("ghaith"));
+	// admin.setEmail("ghaith@email.com");
+	//
+	// userRepository.save(admin);
+	// }
 }
